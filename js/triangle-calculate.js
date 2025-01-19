3ec3121ce324206f027b332cada6dabf96b075be
ApurboShib <shibjoy088@gmail.com>
2025-01-19 13:48:41 +0600

    write area of triabgle function and fetch the b and h value

--- js/triangle-calculate.js
+++ js/triangle-calculate.js
@@ -1,4 +1,31 @@
-console.log('connected');
+/*
+
+objective : get base, height of triangle. calculate the area by using the formula.
+ 1. get base value of the triangle.
+ 2. added an id in the base input feild.
+ 3. use getElementById to access the input feild.
+ 4. get value from the input feild. (nomally we received the value as a string).
+ 5. convert the string value to a number to perform the operations.
+
+
+*/
+
+// console.log('connected');
 function calculateTriangleArea () {
-    console.log('hello from inside the function');
+    // get triangle base value.
+
+    // console.log('hello from inside the function');
+    const triangleBaseInput = document.getElementById('triangle-base'); // base ta niye ashlam 
+    const triangleBaseText = triangleBaseInput.value; // input eh ja value enter kora hbe seta access korlam.
+    const base = parseFloat(triangleBaseText); // convert the sring to flout value.
+    //console.log(triangleBaseText);
+    // check the datatype.
+    // console.log(typeof(triangleBaseText));
+    // console.log(typeof(base));
+
+    // get triangle height.
+    const triangleHeightInput = document.getElementById('triangle-height');
+    const triangleHeightText = triangleHeightInput.value;
+    const height = parseFloat(triangleHeightInput);
+
 }
